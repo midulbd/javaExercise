d4b848a2e56300e1e96f7ba6981e219a8eec98f2
midulbd <midulbd@gmail.com>
2014-09-24 18:54:35 +0600

    intial commint for for bloc of code

---
+++ src/javaexercise/BlockofCode.java
@@ -0,0 +1,23 @@
+package javaexercise;
+
+/**
+ *
+ * @author ASHIQUL
+ */
+public class BlockofCode {
+    public static void main (String args[])
+            {
+                int x,y;
+                y=20;
+                
+                    for (x=0; x<10; x++)
+                        {
+                            System.out.println("x is : " +x);
+                            System.out.println("y is " +y);
+                            y= y-1;
+                                
+                        }
+            
+            }
+    
+}
