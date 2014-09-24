6cc876a4100d47b4300579f3e9a5ab6939a07dce
midulbd <midulbd@gmail.com>
2014-09-24 18:33:11 +0600

    intial commint for for loop

--- src/javaexercise/ForloopTest.java
+++ src/javaexercise/ForloopTest.java
@@ -7,5 +7,13 @@ package javaexercise;
  */
 public class ForloopTest {
     
+    public static void main (String args[])
+    {
+        int x;
+        
+        for (x=0; x<10; x++)
+            System.out.println("This is x: " +x);
+    }
+    
     
 }
