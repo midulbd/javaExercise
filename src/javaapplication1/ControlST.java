45045be1d41a0eaf93980d9247b21f17695986c4
14365001 <14365001@UB40303PC20.bracu.ac.bd>
2014-09-28 19:03:59 +0600

    Initialize control statement

---
+++ src/javaapplication1/ControlST.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author 14365001
+ */
+
+import java.utill.scanner;
+public class Controlst {
+    
+    Scanner scn= new Scanner(System.in);
+     
+    int num=scn.nextInt();
+    
+    if (num>50)
+    {
+        System.out.println("Pass");
+    
+    }
+    
+    else
+    {System.out.println("Fail");}
+    
+}
