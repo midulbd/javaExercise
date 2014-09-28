888cd37bb7647249462855b8b45c7c9cbacea35d
14365001 <14365001@UB40303PC20.bracu.ac.bd>
2014-09-28 19:50:06 +0600

    Initialize class Account

---
+++ src/javaapplication1/Account.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication1;
+
+/**
+ *
+ * @author 14365001
+ */
+public class Account {
+    String name="OOP";
+    int id=1;
+    Double balance=1000.0;
+    
+    
+}
