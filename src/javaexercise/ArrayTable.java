f168178103b9e6e5354205f9ebbb80947564e9ce
midulbd <midulbd@gmail.com>
2014-10-03 10:56:52 +0600

    summing of array content

--- src/javaexercise/ArrayTable.java
+++ src/javaexercise/ArrayTable.java
@@ -11,14 +11,27 @@ package javaexercise;
  * @author ASHIQUL
  */
 public class ArrayTable {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("index\tvalue");
         
         int value[]={12,123,1234,12345,1234156,1234567,1325465456};
-        for (int counter=0; counter<value.length;counter++){
+        for (int counter=0; counter<value.length;counter++)
+        {
             System.out.println(counter+"\t"+value[counter]);
         }
         
+        //summing elements of Arrays
+        
+        int age[]={10,20,30,50,60,70};
+        int sum=0;
+        for(int counter=0; counter<age.length; counter++)
+            {
+                sum+=age[counter];
+                
+            }
+        System.out.println("the sum of the ages is " +sum);
+    
         
     }
     
