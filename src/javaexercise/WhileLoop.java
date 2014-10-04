8708a2ccea1e86291416ec5478238afd6b4e2aed
midulbd <midulbd@gmail.com>
2014-10-04 21:32:53 +0600

    initializing whileloop statement

---
+++ src/javaexercise/WhileLoop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaexercise;
+
+
+public class WhileLoop {
+    public static void main(String args[])
+        {
+            int counter=0;
+            while(counter<11)
+                {
+                    System.out.println(counter);
+                    counter++;
+                }
+        }
+    
+}
