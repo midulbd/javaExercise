935f2ff2b66486ab5b6ef0939e4c97a66fb4ee59
midulbd <midulbd@gmail.com>
2014-10-04 12:12:50 +0600

    practising of arrays

--- src/javaexercise/ArrayEsercise.java
+++ src/javaexercise/ArrayEsercise.java
@@ -24,6 +24,37 @@ public class ArrayEsercise {
                 {
                     System.out.println ("array["+count+"] = "+array[count]);
                 } 
+            
+        
+    
+        
+            int abc[]=new int[15];
+            for (int count=7; count<15;count++)
+                {
+                    abc[count]=count+1;
+                }
+            for (int count=7;count<15; count++)
+                {
+                    System.out.println("array["+count+"]= "+abc[count]);
+                }
+            
+            int xyz[]=new int[20];
+            for (int count=15; count<xyz.length; count++)
+                {
+                    xyz[count]=count+1;
+                    
+                }
+            for (int count=15; count<xyz.length; count++)
+                {
+                    System.out.println("xyz["+count+"]="+xyz[count]);
+                }
+            
+            String tuna[]= new String[3];
+             tuna[0]="tuna chunk";
+             tuna[1]="tuna with tomato";
+             tuna[2]="tuna in sunflower oil";
+            
+             System.out.println(tuna[1]);
         }
     
 }
