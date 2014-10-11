b8a79510b40f2db9d536ea487ee99dc6c773ff98
midulbd <midulbd@gmail.com>
2014-10-11 22:59:28 +0600

    initializing using class object method

---
+++ src/javaexercise/Car.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaexercise;
+
+/**
+ *
+ * @author ASHIQUL
+ */
+public class Car {
+    public void simpleMessage(String name)
+        {
+            System.out.println("im also car "+ name);
+        }
+    
+}
