a86dda71516e21d56d50ba15177638f8d7806a77
Redwana <Redwana@REDWANA-PC>
2014-12-02 09:37:21 +0600

    loop control by more than one variables

---
+++ src/myproject/LoopControlByTwoVAriable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject;
+
+/**
+ *
+ * @author Redwana
+ */
+public class LoopControlByTwoVAriable {
+    public static void main (String args[])
+    {
+        int a,b;
+        for ( a =1, b = 4; a < b; a++, b--)
+        {
+            System.out.println("value of a = "+a);
+            System.out.println("value of b = "+b);
+        }
+    }
+    
+}
